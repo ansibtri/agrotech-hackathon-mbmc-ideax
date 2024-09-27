88d60c5c834559eefd139a9bc9d56d49603ba195
ansibtri <ansibtri961@gmail.com>
2024-09-28 00:43:13 +0545

    bug fix in register function

--- client/script/register.js
+++ client/script/register.js
@@ -2,13 +2,14 @@
 // script to handle registration of new users
 
 const SERVER_URL = "http://localhost:5173/api/";
-REGISTER_URL = `${SERVER_URL}auth/register`;
+const REGISTER_URL = `${SERVER_URL}auth/register`;
 
 const registerButton = document.getElementById("registerButton");
 registerButton.addEventListener("click", register);
 
-function register(e) {
+async function register(e) {
   e.preventDefault();
+  e.stopPropagation();
   const firstName = document.getElementById("firstName");
   const lastName = document.getElementById("lastName");
   const email = document.getElementById("email");
@@ -16,34 +17,45 @@ function register(e) {
   const contact = document.getElementById("contact");
   const category = document.getElementById("category");
   const confirmPassword = document.getElementById("confirmPassword");
-  const error = document.getElementById("error");
+  const message = document.getElementById("message");
+  
+  console.log(firstName.value, lastName.value, email.value, password.value, contact.value, category.value, confirmPassword.value);
 
   if (password.value !== confirmPassword.value) {
-    error.innerHTML = "Passwords do not match";
+    message.innerHTML = "Passwords do not match";
     return;
   }
-  error.innerHTML = "";
-  registerButton.disabled = true;
-  const response = fetch(REGISTER_URL, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      firstName: firstName.value,
-      lastName: lastName.value,
-      email: email.value,
-      password: password.value,
-      contact: contact.value,
-      category: category.value,
-    }),
-  });
-
-      if (response.status === 200) {
-      window.location.href = "/login";
-      } else {
-      error.innerHTML = "An error occurred";
-      }
-      registerButton.disabled = false;
-
+  // send data to server
+  const data = {
+    firstName: firstName.value,
+    lastName: lastName.value,
+    email: email.value,
+    password: password.value,
+    contact: contact.value,
+    category: category.value,
+  };
+  
+  try {
+    await fetch(REGISTER_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((response) => {
+        console.log(response["status"]);
+        if (response["status"] === 200) {
+          message.innerHTML = response["message"];
+        } else {
+          message.innerHTML = response["message"];
+        }
+      })
+      .catch((err) => {
+        message.innerHTML = err.message;
+      });
+  } catch (error) {
+    message.innerHTML = error.message;
+  }
 }
