165100d6c03c806eb4385990358e214e8b8f84af
ansibtri <ansibtri961@gmail.com>
2024-09-27 15:28:47 +0545

    login route build finish

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ require("dotenv").config()
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-
+const morgan = require("morgan");
 const { connect } = require("./Database.js");
 const Auth = require("./routes/Auth");
 connect()
 
 const PORT = process.env.PORT || 3000;
 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(morgan("common"))
 app.use(bodyParser.json());
 app.use('/api/auth', Auth); // setting up routes
 
--- routes/Auth.js
+++ routes/Auth.js
@@ -3,9 +3,10 @@ const User = require("../models/User");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
-const {reponse} = require("../utils/ResponseHandlers");
-const response = require("../utils/ResponseHandlers");
 
+const response = require("../utils/ResponseHandlers"); // response handler
+
+// register route
 router.post("/register", async (req, res) => {
   try {
     const { firstName, lastName, email, password, contact } = req.body; // getting user data
@@ -28,5 +29,26 @@ router.post("/register", async (req, res) => {
   }
 });
 
+
+// login route
+router.post('/login', async (req,res)=>{
+      try{
+            const {email,password} = req.body; // getting user data
+
+            const checkUserExistence = await User.findOne({email:email}).exec(); // checking if user exists
+            if(checkUserExistence !== null && checkUserExistence['email'] !== undefined && checkUserExistence['email'] === email){
+                  const checkUserPassword = await bcrypt.compare(password.toString(), checkUserExistence['password']); // comparing password
+                  if(checkUserPassword){
+                        const token = jwt.sign({email:email, userId: checkUserExistence['_id']}, process.env.JWT_SECRET); // generating token
+                        response(res, 200, "Logged In Successfully!!!", {token:token}); // sending token
+                  }
+            }else{
+                  response(res, 200, "User not found", null); // user not found
+            }
+      }catch(error){
+            console.log(error) // logging error
+            response(res, 500, "Internal server error", {error: error.message}); // sending error
+      } 
+})
 
 module.exports = router;
