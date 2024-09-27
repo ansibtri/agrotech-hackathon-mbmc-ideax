ba14ae3cc53ba15540c71e127dfee0ff9bfd8d7f
ansibtri <ansibtri961@gmail.com>
2024-09-27 18:34:12 +0545

    category field added

--- models/User.js
+++ models/User.js
@@ -22,6 +22,10 @@ const UserSchema = new mongoose.Schema({
       contact:{
             type:String,
             required:true
+      },
+      category:{
+            type:String,
+            required:true
       }
 },{
       timestamps:true
